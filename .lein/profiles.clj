efefd7ce2bdd9394b8adf684a1e285c15f817910
Shintaro Matsushima <shintaromatsushima@gmail.com>
2017-06-29 21:53:45 +0900

    Added plugins for code analysis and linter

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,6 @@
-{:repl {:plugins [[refactor-nrepl "2.2.0"]
+{:user {:plugins [[jonase/eastwood "0.2.4"]
+                  [lein-kibit "0.1.5"]]}
+ :repl {:plugins [[refactor-nrepl "2.3.1"]
                   [cider/cider-nrepl "0.14.0"]]
         :dependencies [[alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.12"]]}}
